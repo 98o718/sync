582d3e5c5bc51139b099b15b28ccbe8312f12c4f
98o718 <kalyakin.maks@mail.ru>
2020-04-27 22:07:40 +0300

    Add Travis CD

---
+++ server/ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps: [
+    {
+      name: 'sync-server',
+      script: 'index.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+        PORT: 3388,
+      },
+    },
+  ],
+
+  deploy: {
+    production: {
+      user: 'travis',
+      host: '51.15.124.103',
+      ref: 'origin/master',
+      repo: 'git@github.com:98o718/sync.git',
+      path: '/home/travis/sync-server',
+      'post-deploy':
+        'cd server && yarn && pm2 reload ecosystem.config.js --env production',
+    },
+  },
+}
