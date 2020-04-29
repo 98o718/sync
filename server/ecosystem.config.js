540b6af4100ac5f94c0b7b15f5ff2a0d4c913aaf
98o718 <kalyakin.maks@mail.ru>
2020-04-29 05:09:42 +0300

    Change deploy branch

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -21,7 +21,7 @@ module.exports = {
     production: {
       user: 'travis',
       host: '51.15.124.103',
-      ref: 'origin/master',
+      ref: 'origin/webrtc',
       repo: 'git@github.com:98o718/sync.git',
       path: '/home/travis/sync-server',
       'post-deploy':
